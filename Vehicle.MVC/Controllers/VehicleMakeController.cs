f8aaf77f632866b42d53ac10dd8e0519ba051615
Ivan <ivan1501@gmail.com>
2020-02-04 09:19:12 +0100

    Added view controller for VehicleMake and VehicleModel

---
+++ Vehicle.MVC/Controllers/VehicleMakeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vehicle.DAL;
+using Vehicle.DAL.Entities;
+
+namespace Vehicle.MVC.Controllers
+{
+    public class VehicleMakeController : Controller
+    {
+        private VehicleContext db = new VehicleContext();
+
+        // GET: VehicleMake
+        public ActionResult Index()
+        {
+            return View(db.VehicleMakes.ToList());
+        }
+
+        // GET: VehicleMake/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMake/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleMake/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicleMake.Id = Guid.NewGuid();
+                db.VehicleMakes.Add(vehicleMake);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMake/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMake/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicleMake).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMake/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMake/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            db.VehicleMakes.Remove(vehicleMake);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Vehicle.MVC/Controllers/VehicleModelController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vehicle.DAL;
+using Vehicle.DAL.Entities;
+
+namespace Vehicle.MVC.Controllers
+{
+    public class VehicleModelController : Controller
+    {
+        private VehicleContext db = new VehicleContext();
+
+        // GET: VehicleModel
+        public ActionResult Index()
+        {
+            var vehicleModels = db.VehicleModels.Include(v => v.VehicleMake);
+            return View(vehicleModels.ToList());
+        }
+
+        // GET: VehicleModel/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModel/Create
+        public ActionResult Create()
+        {
+            ViewBag.VehicleMakeId = new SelectList(db.VehicleMakes, "Id", "Name");
+            return View();
+        }
+
+        // POST: VehicleModel/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Abrv,VehicleMakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicleModel.Id = Guid.NewGuid();
+                db.VehicleModels.Add(vehicleModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.VehicleMakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.VehicleMakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModel/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.VehicleMakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.VehicleMakeId);
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModel/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Abrv,VehicleMakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicleModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.VehicleMakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.VehicleMakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModel/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            db.VehicleModels.Remove(vehicleModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
